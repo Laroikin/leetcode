1201ca80fc3753f770304dba3e28e3d87c978ea1
Laroikin <said24abdi04@gmail.com>
2025-04-14 17:57:03 +0900

    feat: add can-place-flowers.ts

---
+++ leetcode-75/can-place-flowers.ts
@@ -0,0 +1,20 @@
+// Complexity: O(n)
+// https://leetcode.com/problems/can-place-flowers/submissions/1606408934/?envType=study-plan-v2&envId=leetcode-75
+
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  let count = 0;
+  for (let ind = 0; ind < flowerbed.length; ind++) {
+    if (
+      (flowerbed[ind - 1] === 0 || flowerbed[ind - 1] === undefined) &&
+      flowerbed[ind] === 0 &&
+      (flowerbed[ind + 1] === 0 || flowerbed[ind + 1] === undefined)
+    ) {
+      flowerbed[ind] = 1;
+      count++;
+    }
+  }
+
+  return n <= count;
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2));
