bcf65996f8eaf2f7d0225e7ed6247aa15b549281
Laroikin <said24abdi04@gmail.com>
2025-04-16 00:07:43 +0900

    feat: add defuse-the-bomb

--- dailies/defuse-the-bomb.ts
+++ dailies/defuse-the-bomb.ts
@@ -1,31 +1,36 @@
 // Difficulty: Easy
-// Complexity: O(n^3)
-// Link: https://leetcode.com/problems/count-good-triplets/submissions/1606679654/?envType=daily-question&envId=2025-04-14
+// Complexity: O(n)
+// Link: https://leetcode.com/problems/defuse-the-bomb/?envType=daily-question&envId=2025-04-15
 
-function countGoodTriplets(
-  arr: number[],
-  a: number,
-  b: number,
-  c: number
-): number {
-  let triplets = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      for (let k = 0; k < arr.length; k++) {
-        if (
-          Math.abs(arr[i] - arr[j]) <= a &&
-          Math.abs(arr[j] - arr[k]) <= b &&
-          Math.abs(arr[i] - arr[k]) <= c &&
-          i < j &&
-          j < k
-        ) {
-          triplets++;
-        }
-      }
-    }
+function decrypt(code: number[], k: number): number[] {
+  let ans = new Array(code.length).fill(0);
+
+  if (k == 0) {
+    return ans;
   }
 
-  return triplets;
+  let start = 1;
+  let end = k;
+  let sum = 0;
+
+  if (k < 0) {
+    start = code.length - Math.abs(k);
+    end = code.length - 1;
+  }
+
+  for (let i = start; i <= end; i++) {
+    sum += code[i];
+  }
+
+  for (let i = 0; i < code.length; i++) {
+    ans[i] = sum;
+    sum -= code[start % code.length];
+    sum += code[(end + 1) % code.length];
+    start++;
+    end++;
+  }
+
+  return ans;
 }
 
-console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3));
+console.log(decrypt([2, 4, 9, 3], -2));
